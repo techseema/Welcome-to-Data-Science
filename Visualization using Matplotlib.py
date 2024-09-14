ab6640eeb194527801f8e3e60ef4f280b43826dc
techseema <tech.seema22@gmail.com>
2024-09-14 22:58:16 +0530

    first

---
+++ Visualization using Matplotlib.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+# Sample data
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 5, 3, 6]
+
+# Create a line plot
+plt.plot(x, y)
+
+# Add labels and a title
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Sample Line Plot')
+
+# Show the plot
+plt.show()
