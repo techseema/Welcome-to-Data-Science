fb7b1725ec059f9c65056a7fea27d3eb06537bdf
techseema <tech.seema22@gmail.com>
2024-09-14 22:39:33 +0530

    first

---
+++ Housing Rent Prediction.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+df=pd.read_csv("USA_Housing.csv")
+df.head(10)
+df.info()
+df.describe()
+df.columns
+sns.pairplot(df)
+#sns.heatmap(df.corr(),annot=True)
